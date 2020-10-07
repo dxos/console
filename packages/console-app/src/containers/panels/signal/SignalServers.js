f2440db40fc0fb018c657dd5ccdc4d868c86e98a
Thomas E Lackey <telackey@bozemanpass.com>
2020-10-07 11:59:18 -0500

    Unused imports.

--- packages/console-app/src/containers/panels/signal/SignalServers.js
+++ packages/console-app/src/containers/panels/signal/SignalServers.js
@@ -14,13 +14,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Box from '@material-ui/core/Box';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-
 import Table from '../../../components/Table';
 import TableCell from '../../../components/TableCell';
 import NetworkGraph from '../../../components/NetworkGraph';
