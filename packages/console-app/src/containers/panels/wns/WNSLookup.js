93f10bf2166d44a8aef861ff56368a55b076396f
Thomas E Lackey <telackey@bozemanpass.com>
2020-10-19 17:03:04 -0500

    Add Authorities, autocomplete, and theme.

--- packages/console-app/src/containers/panels/wns/WNSLookup.js
+++ packages/console-app/src/containers/panels/wns/WNSLookup.js
@@ -3,30 +3,93 @@
 //
 
 import React, { useContext, useState } from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
+import NativeSelect from '@material-ui/core/NativeSelect';
 import TableBody from '@material-ui/core/TableBody';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
 
-import { ConsoleContext, useRegistry } from '../../../hooks';
+import { useQuery } from '@apollo/react-hooks';
+
 import Json from '../../../components/Json';
 import Table from '../../../components/Table';
 import TableCell from '../../../components/TableCell';
+import { ConsoleContext, useQueryStatusReducer, useRegistry } from '../../../hooks';
+
+import WNS_RECORDS from '../../../gql/wns_records.graphql';
 
 const WNSLookup = () => {
   const { config } = useContext(ConsoleContext);
   const { registry } = useRegistry(config);
   const [result, setResult] = useState({});
+  const [inputValue, setInputValue] = useState('');
+  const [lookupType, setLookupType] = useState('wrn');
+
+  const data = useQueryStatusReducer(useQuery(WNS_RECORDS, {
+    pollInterval: config.api.intervalQuery
+  }));
+
+  if (!data) {
+    return null;
+  }
+  const records = JSON.parse(data.wns_records.json);
+
+  const getNames = () => {
+    let ret;
+    switch (lookupType) {
+      case 'wrn': {
+        ret = [];
+        records.forEach(item => ret.push(...item.names));
+        break;
+      }
+      case 'authority': {
+        // Use the known names to come up with a default list of authorities.
+        // TODO(telackey): Should we be able to query WNS for a list of authorities?
+        const names = new Set();
+        for (const record of records) {
+          for (const name of record.names) {
+            // TODO(telackey): We need a general purpose WRN handling library.
+            names.add(name.replace('wrn://', '').split('/')[0]);
+          }
+        }
+        ret = Array.from(names.values());
+        break;
+      }
+      default:
+        throw new Error(`Unrecognized lookup type: ${lookupType}`);
+    }
+    ret.sort();
+    return ret;
+  };
+
+  const handleSelect = (evt) => {
+    evt.preventDefault();
+
+    setLookupType(evt.target.value);
+  };
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    const formData = new FormData(evt.target);
 
-    const wrn = formData.get('wrn');
-    if (wrn) {
-      const history = formData.get('wrnhistory') === 'on';
-      const result = await registry.lookupNames([wrn], history);
-      setResult(result);
+    if (!inputValue) {
+      setResult('');
+      return;
     }
+
+    let result;
+    switch (lookupType) {
+      case 'wrn':
+        result = await registry.lookupNames([inputValue], true);
+        break;
+      case 'authority':
+        result = await registry.lookupAuthorities([inputValue]);
+        break;
+      default:
+        throw new Error(`Unrecognized lookup type: ${lookupType}`);
+    }
+    setResult(result);
   };
 
   return (
@@ -36,28 +99,34 @@ const WNSLookup = () => {
           <Table>
             <TableBody>
               <TableRow>
-                <TableCell align='left' width='25%'>
-                  <label htmlFor='wrn'>WRN</label>
+                <TableCell align='left' width='150px' style={{ verticalAlign: 'middle' }}>
+                  <NativeSelect id='lookupType' name='lookupType' defaultValue='wrn' onChange={handleSelect}>
+                    <option value='authority'>Authority</option>
+                    <option value='wrn'>WRN</option>
+                  </NativeSelect>
                 </TableCell>
                 <TableCell align='left'>
-                  <input
-                    type='text'
-                    id='wrn'
-                    name='wrn'
-                    size={75}
+                  <Autocomplete
+                    options={getNames()}
+                    autoFocus
+                    name='value'
+                    id='value'
+                    fullWidth
+                    freeSolo
+                    inputValue={inputValue}
+                    onInputChange={(event, newInputValue) => {
+                      setInputValue(newInputValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} variant='outlined' />}
                   />
-                  <input
-                    type='checkbox'
-                    id='wrnhistory'
-                    name='wrnhistory'
-                  />
-                  <label htmlFor='wrnhistory'>History?</label>
+                </TableCell>
+                <TableCell align='left' width='150px' style={{ verticalAlign: 'middle' }}>
+                  <Button variant='contained' color='primary' type='submit'>Search</Button>
                 </TableCell>
               </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
-        <button>Lookup</button>
       </form>
       <Json data={result} />
     </div>
