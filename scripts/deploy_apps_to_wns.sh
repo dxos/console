e78184c0c319fc1fa08d320fc760bc9783a214b5
Thomas E Lackey <telackey@bozemanpass.com>
2020-11-16 14:11:34 -0600

    fix: add repo link

--- scripts/deploy_apps_to_wns.sh
+++ scripts/deploy_apps_to_wns.sh
@@ -7,32 +7,15 @@ for appdir in `find ./packages -name '*-app' -type d | grep -v node_modules`; do
 
   WNS_ORG="dxos"
   PKG_NAME=`cat package.json | jq -r '.name' | cut -d'/' -f2- | sed 's/-app$//'`
-  PKG_DESC=`cat package.json | jq -r '.description'`
-  PKG_VERSION=`cat package.json | jq -r '.version'`
-  
-  if [ -z "$PKG_DESC" ]; then
-    PKG_DESC="$PKG_NAME"
-  fi
-  
   WNS_NAME="$WNS_ORG/$PKG_NAME"
-  WNS_VERSION=`yarn -s wire wns name resolve wrn://${WNS_ORG}/application/${PKG_NAME} | jq -r '.records[0].attributes.version'`
-  
-  if [ -z "$WNS_VERSION" ]; then
-    WNS_VERSION="0.0.1"
-  fi
   
   cat <<EOF > app.yml
 name: $PKG_NAME
 build: yarn dist
-version: $WNS_VERSION
 EOF
   
   yarn clean
-
-  # TODO(telackey): We need to fix `wire app build` not to bake in a path!
-  # In the meantime, use `yarn dist` which will not.
-  # yarn -s wire app build
-  yarn dist
+  yarn -s wire app build
 
   if [ -d "dist/production" ]; then
     yarn -s wire app publish --path './dist/production'
