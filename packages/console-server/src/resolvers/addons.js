5b632a9a46c0b157cc94d8b81bd291a457b20f14
Thomas E Lackey <thomas@wireline.io>
2020-12-09 18:45:09 -0600

    fix: detect running addon and add their links to the console sidebar (#57)

---
+++ packages/console-server/src/resolvers/addons.js
@@ -0,0 +1,37 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import childProcess from 'child_process';
+
+// TODO(telackey): Make pluggable.
+const ifRadicle = () => {
+  try {
+    const result = childProcess.execSync('docker ps -f "ancestor=dxos/radicle-seed-node" -q');
+    if (result) {
+      return { title: 'Radicle', url: '/' };
+    }
+  } catch (e) {}
+};
+
+// TODO(telackey): Use the local Sentry.
+const ifSentry = () => {
+  return {
+    title: 'Sentry',
+    url: 'http://sentry.kube.dxos.network:9000/'
+  };
+};
+
+export const addonResolvers = {
+  Query: {
+    addon_list: async (_, __, { config }) => {
+      return {
+        timestamp: new Date().toUTCString(),
+        json: JSON.stringify([
+          ifSentry(),
+          ifRadicle()
+        ].filter(x => x))
+      };
+    }
+  }
+};
