b4297b3ccf582136d21b5f5e80419b3420f154d3
Egor Gripasov <gripasovegor@gmail.com>
2020-12-10 13:55:26 +0300

    fix: add version link to bots records.

--- packages/console-app/src/containers/panels/bots/BotRecords.js
+++ packages/console-app/src/containers/panels/bots/BotRecords.js
@@ -12,6 +12,7 @@ import { ConsoleContext, useQueryStatusReducer, useSorter } from '../../../hooks
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableBody from '@material-ui/core/TableBody';
+import Link from '@material-ui/core/Link';
 
 import Table from '../../../components/Table';
 import TableCell from '../../../components/TableCell';
@@ -38,17 +39,32 @@ const BotRecords = () => {
           <TableCell onClick={sortBy('names[0]')}>Identifier</TableCell>
           <TableCell onClick={sortBy('attributes.version')} size='small'>Version</TableCell>
           <TableCell onClick={sortBy('attributes.name')}>Name</TableCell>
+          <TableCell onClick={sortBy('attributes.repository')}>Repository</TableCell>
           <TableCell onClick={sortBy('createTime')} size='small'>Created</TableCell>
           <TableCell size='icon' />
         </TableRow>
       </TableHead>
       <TableBody>
-        {records.sort(sorter).map(({ id, names, createTime, attributes: { name: displayName, version } }) => {
+        {records.sort(sorter).map(({
+          id, names, createTime, attributes: { name: displayName, version, versionUrl, repositoryVersion, repository, homepage }
+        }) => {
+          const url = repository || homepage;
+          if (!versionUrl && repository && repositoryVersion && repository.includes('github')) {
+            versionUrl = `${repository}/tree/${repositoryVersion}`.replace('-dirty', '');
+          }
           return (
             <TableRow key={id} size='small'>
               <TableCell monospace>{names.map(name => <div key={name}>{name}</div>)}</TableCell>
-              <TableCell monospace>{version}</TableCell>
+              <TableCell monospace>
+                {versionUrl
+                  ? <Link href={versionUrl}>{version}</Link>
+                  : version}
+              </TableCell>
               <TableCell>{displayName}</TableCell>
+              <TableCell>
+                {url &&
+                  <Link href={url} target={url}>{url}</Link>}
+              </TableCell>
               <TableCell>{moment.utc(createTime).fromNow()}</TableCell>
               <TableCell />
             </TableRow>
