fe16ced800e79660a7464b4b8c485b954d5edc12
richburdon <rich.burdon@gmail.com>
2020-12-19 15:01:03 +0000

    Path.

--- packages/console-app/src/main.js
+++ packages/console-app/src/main.js
@@ -15,7 +15,12 @@ debug.enable(config.system.debug);
 // https://webpack.js.org/guides/progressive-web-application
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    // TODO(burdon): Remove "wrn:" from registry path so encoding is not required.
+    // TODO(burdon): Remove "wrn:" from path so that escape characters are not required.
+    // main.js?56d7:23 SW registration failed: TypeError: Failed to register a ServiceWorker:
+    // The provided scope ('https://apollo1.kube.moon.dxos.network/app/wrn%3A%2F%2Fdxos%2Fapplication%2Fconsole%40alpha/')
+    // or scriptURL ('https://apollo1.kube.moon.dxos.network/app/wrn%3A%2F%2Fdxos%2Fapplication%2Fconsole%40alpha/service_worker.js')
+    // includes a disallowed escape character.
+    //     at eval (main.js?56d7:20)
     const path = window.location.pathname + 'service_worker.js';
     navigator.serviceWorker.register(path).then(registration => {
       console.log('SW registered:', registration);
