2f4b05b0e059d7085da47edf2278eeb4d2b523a4
Rich Burdon <rich.burdon@gmail.com>
2021-08-26 17:44:31 -0400

    Router.

--- packages/konsole-app/src/containers/Sidebar.tsx
+++ packages/konsole-app/src/containers/Sidebar.tsx
@@ -2,28 +2,39 @@
 // Copyright 2020 DXOS.org
 //
 
-import React from 'react';
+import React, { ComponentType } from 'react';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 
 export interface IPanel {
+  path: string
   label: string
-  component: object
+  component: ComponentType
 }
 
 interface SidebarProperties {
   panels: IPanel[]
+  selected: string | undefined
+  onSelect: (path: string) => void
 }
 
 /**
  * Generic sidebar panel.
  * @constructor
  */
-export const Sidebar = ({ panels }: SidebarProperties) => {
+export const Sidebar = ({ panels, selected, onSelect }: SidebarProperties) => {
+
+  console.log('::::', selected);
+
   return (
     <div>
       <List>
-        {panels.map(({ label }, i) => (
-          <ListItem key={i} button>
+        {panels.map(({ path, label }, i) => (
+          <ListItem
+            key={i}
+            button
+            selected={path === selected}
+            onClick={() => onSelect(path)}
+          >
             <ListItemText
               primary={label}
             />
--- packages/konsole-app/src/main.tsx
+++ packages/konsole-app/src/main.tsx
@@ -4,11 +4,20 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  useHistory,
+  useParams
+} from 'react-router-dom';
 
 import { Container, IPanel, Root, Sidebar } from './containers';
 import { ConfigContext, IConfig, RegistryContext } from './hooks';
 import { ConfigPanel, RecordPanel } from './panels';
 import { MockRegistryClient } from './testing';
+import { Config } from '../stories/panels.stories';
 
 // TODO(burdon): Load from environment.
 const config: IConfig = {
@@ -20,27 +29,58 @@ const config: IConfig = {
 
 const panels: IPanel[] = [
   {
+    path: '/config',
     label: 'Config',
     component: ConfigPanel
   },
   {
+    path: '/records',
     label: 'Records',
     component: RecordPanel
   }
 ];
 
-// TODO(burdon): Router: https://reactrouter.com/web/api/Hooks
+const App = () => {
+  const history = useHistory();
+  const { panel }: { panel: string } = useParams();
+
+  return (
+    <Root>
+      <Container
+        sidebar={
+          <Sidebar
+            panels={panels}
+            selected={`/${panel}`}
+            onSelect={path => history.push(path)}
+          />
+        }
+      >
+        <>
+          {panels.map(({ path, component }) => (
+            <Route
+              key={path}
+              path={path}
+              component={component}
+            />
+          ))}
+        </>
+      </Container>
+    </Root>
+  )
+};
+
 const start = (config: IConfig) => {
   ReactDOM.render((
     <ConfigContext.Provider value={config}>
       <RegistryContext.Provider value={new MockRegistryClient()}>
-        <Root>
-          <Container
-            sidebar={<Sidebar panels={panels} />}
-          >
-            <ConfigPanel />
-          </Container>
-        </Root>
+        <Router>
+          <Switch>
+            <Route path='/:panel'>
+              <App />
+            </Route>
+            <Redirect to={panels[0].path} />
+          </Switch>
+        </Router>
       </RegistryContext.Provider>
     </ConfigContext.Provider>
   ), document.getElementById('root'));
