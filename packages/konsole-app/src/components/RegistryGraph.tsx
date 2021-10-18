f1836d68167224abbfa9883bea4882bfd81eaa6a
rzadp <roopert7@gmail.com>
2021-10-18 17:52:14 +0200

    WIP

--- packages/konsole-app/src/components/RegistryGraph.tsx
+++ packages/konsole-app/src/components/RegistryGraph.tsx
@@ -5,12 +5,33 @@
 import { Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import useResizeAware from 'react-resize-aware';
+import { colors } from '@mui/material';
 
 import { SVG, convertTreeToGraph, createTree, useGrid, GraphType } from '@dxos/gem-core';
-import { ForceLayout, Graph } from '@dxos/gem-spore';
+import { ForceLayout, Graph, NodeProjector } from '@dxos/gem-spore';
 import { Domain, Resource } from '@dxos/registry-client';
 
 import { IRecord } from './RecordsTable';
+import { makeStyles } from '@mui/styles';
+
+const nodeColors: (keyof typeof colors)[] = ['red', 'green', 'blue', 'yellow', 'orange', 'grey'];
+
+const useStyles = makeStyles(() => ({
+  nodes: nodeColors.reduce((map: any, color: string) => {
+    map[`& g.node.${color} circle`] = {
+      fill: (colors as any)[color][400],
+      stroke: (colors as any)[color][700]
+    };
+
+    map[`& g.node.${color} text`] = {
+      fontFamily: 'sans-serif',
+      fontSize: 12,
+      fill: colors['grey'][700]
+    };
+
+    return map;
+  }, {})
+}));
 
 interface RegistryGraphProps {
   domains?: Domain[]
@@ -24,8 +45,22 @@ export const RegistryGraph = ({ domains = [], records = [], resources = [] }: Re
   const grid = useGrid(size);
   const [data, setData] = useState<GraphType>({links: [], nodes: []});
   const [layout] = useState(() => new ForceLayout({ force: { links: { distance: 80 } } }));
+  const classes = useStyles();
 
   console.log({domains, records, resources, data})
+
+  const [{ nodeProjector }] = useState({
+    nodeProjector: new NodeProjector({
+      node: {
+        showLabels: true,
+        propertyAdapter: (node: any) => {
+          return {
+            class: 'grey'
+          };
+        }
+      }
+    })
+  });
 
   useEffect(() => {
     const resourceNodes: GraphType['nodes'] = resources.map(resource => ({id: resource.id.toString(), title: resource.id.toString()}))
@@ -49,6 +84,10 @@ export const RegistryGraph = ({ domains = [], records = [], resources = [] }: Re
           grid={grid}
           data={data}
           layout={layout}
+          nodeProjector={nodeProjector}
+          classes={{
+            nodes: classes.nodes
+          }}
         />
       </SVG>
     </Box>
