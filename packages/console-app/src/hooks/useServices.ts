d810335e204eafa7696fda4f1d945d9034edb63c
egorgripasov <gripasovegor@gmail.com>
2022-02-03 16:57:44 -0500

    fix: services actions.

--- packages/console-app/src/hooks/useServices.ts
+++ packages/console-app/src/hooks/useServices.ts
@@ -4,9 +4,19 @@
 
 import urlJoin from 'proper-url-join';
 
+import { ConfigV1Object } from '@dxos/config';
+
 import { IService } from '../types';
 import { useConfig } from './useConfig';
-import { useRequest } from './useRequest';
+import { useRequest, httpRequester } from './useRequest';
+
+export const serviceActionRequester = async (config: ConfigV1Object, service: string, action: string) => {
+  return httpRequester({
+    url: urlJoin(config.runtime?.services?.app?.server, config.runtime?.services?.kube?.endpoints?.services),
+    method: 'POST',
+    params: { service, action }
+  });
+};
 
 export const useServices = (cached = true) => {
   const config = useConfig();
