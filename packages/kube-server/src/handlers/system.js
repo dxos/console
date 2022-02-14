ec5ad28bfa7fa612e16bd94714d1b43c891b79b0
egorgripasov <gripasovegor@gmail.com>
2022-02-14 12:37:10 -0500

    fix: add lock as well.

--- packages/kube-server/src/handlers/system.js
+++ packages/kube-server/src/handlers/system.js
@@ -6,6 +6,8 @@ import { spawnSync } from 'child_process';
 import { existsSync, readFileSync } from 'fs';
 import NodeCache from 'node-cache';
 
+import { Lock } from '@dxos/async';
+
 const CACHE_TTL = 10;
 
 const FAILED = 'failed';
@@ -17,6 +19,7 @@ const KUBE_SERVICES_INFO_FILE = '/opt/kube/services.json';
 const ALLOWED_ACTIONS = ['restart'];
 
 const _cache = new NodeCache({ stdTTL: CACHE_TTL, useClones: false });
+const lock = new Lock();
 
 const mergeByProperty = (target, source, prop) => {
   source.forEach(sourceElement => {
@@ -27,13 +30,7 @@ const mergeByProperty = (target, source, prop) => {
   });
 };
 
-export const getServiceInfo = async ({ usage = false, cached = true }) => {
-  const cacheKey = `usage-${usage}`;
-  const cachedServices = cached && _cache.get(cacheKey);
-  if (cachedServices) {
-    return cachedServices;
-  }
-
+const getServices = (usage) => {
   const command = 'dx';
   const args = ['service', '--json', '--usage', usage];
 
@@ -46,10 +43,20 @@ export const getServiceInfo = async ({ usage = false, cached = true }) => {
   }
 
   mergeByProperty(runningServices, servicesInfo, 'name');
-
-  cached && _cache.set(cacheKey, runningServices);
-
   return runningServices;
+};
+
+export const getServiceInfo = async ({ usage = false, cached = true }) => {
+  const cacheKey = `usage-${usage}`;
+  const cachedServices = cached && _cache.get(cacheKey);
+  if (cachedServices) {
+    return cachedServices;
+  }
+
+  const services = await lock.executeSynchronized(async () => getServices(usage));
+  cached && _cache.set(cacheKey, services);
+
+  return services;
 };
 
 export const runServiceAction = async ({ action, service }) => {
